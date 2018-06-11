82f4c0a0faf6dd7e388e6e73a3ed4b218d72db9b
michaellennox <michaelctlennox@gmail.com>
2018-06-11 18:12:21 +0100

    Adds login and logout elements to a basic navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get '/', to: 'home#show', as: 'home'
   resources :groups, only: %i[new create show], param: :url_slug
   resources :users, only: %i[new create]
+
+  get 'login', to: 'sessions#new', as: 'sessions'
+  post 'login', to: 'sessions#create', as: 'new_session'
+  delete 'logout', to: 'sessions#destroy', as: 'session'
 end
