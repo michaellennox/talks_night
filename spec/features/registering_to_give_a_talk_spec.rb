4412ee3862febc0b05e74107936467ecd7d88004
Michael Canden-Lennox <michael.lennox@fundingcircle.com>
2018-08-09 23:11:20 +0100

    Add the Groups::TalksSuggestions controller for suggesting a talk
    This commit adds the final part of the signing up to give a talk flow which is to have a place to capture how a group organiser should be getting in contact with a speaker. This creates the TalkSuggestion linking the Talk and Group together and includes that contact info. Follow on branches will include functionality for speakers to see and edit talks they have created/suggested as well as group organisers to approve speakers for specific events.

--- spec/features/registering_to_give_a_talk_spec.rb
+++ spec/features/registering_to_give_a_talk_spec.rb
@@ -25,6 +25,9 @@ RSpec.feature 'Registering to give a talk', type: :feature do
 
     expect(page).to have_current_path new_group_talk_suggestion_path(group, talk_id: Talk.last.id)
 
-    # assert page shows speaker tools
+    fill_in "We're looking forwards to speaking to you! How shall we get in touch?", with: 'My contact is foobar'
+    click_on 'Propose Talk'
+
+    expect(page).to have_current_path group_path(group)
   end
 end
